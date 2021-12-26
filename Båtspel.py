b3f77d238706ed58b082e6f4107759b197d7062d
Omer69420 <omer.cinar@skola.botkyrka.se>
2021-12-26 20:34:14 +0100

    turtleturtle

--- Båtspel.py
+++ Båtspel.py
@@ -1,6 +1,12 @@
-import pygame
-pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.update()
-pygame.quit()
-quit()
+#import pygame
+#pygame.init()
+#dis=pygame.display.set_mode((400,300))
+#pygame.display.update()
+#pygame.quit()
+#quit()
+
+
+
+import turtle
+t = turtle.Turtle()
+t.circle(50)
