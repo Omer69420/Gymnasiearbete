aee6912a1150869d90e26671d8e2c63121bf0c50
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2021-12-26 21:05:08 +0100

    display_screen

--- Båtspel.py
+++ Båtspel.py
@@ -1,1 +1,20 @@
+import pygame
 
+pygame.init()
+
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Båtspel')
+
+run = True
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
