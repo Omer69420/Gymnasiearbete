a82281e17fb6e186a93f3ad69922a8dc2a5f04ac
Charlie Gador <charlie.gador@skola.botkyrka.se>
2021-12-27 21:38:25 +0100

    bakgrund

--- Båtspel.py
+++ Båtspel.py
@@ -9,11 +9,13 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Båtspel')
 
 
+background = pygame.image.load(r'images\baby-blue-color-solid-background-1920x1080.png')
 boat = pygame.image.load(r'images\gymnasiearbete2d.png')
 
 run = True
 while run:
 
+    screen.blit(background, (0, 0))
     screen.blit(boat, (100, 100))
 
     for event in pygame.event.get():
