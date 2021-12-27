5e94011fffd11065e24ee7b4de1cd30e20f0493d
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2021-12-27 21:06:53 +0100

    båt_image

--- Båtspel.py
+++ Båtspel.py
@@ -1,5 +1,5 @@
 import pygame
-
+from pygame.locals import *
 pygame.init()
 
 screen_width = 700
@@ -8,8 +8,13 @@ screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Båtspel')
 
+
+boat = pygame.image.load(r'C:\Users\Eddin\Desktop\Python\gymnasiearbete2d.png')
+
 run = True
 while run:
+
+    screen.blit(boat, (100, 100))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
